c311d80467fd9d9224861ecc36be31cb64fa0a2b
Diego Castro <diego_castro_soto@hotmail.com>
2023-08-23 13:54:03 -0500

    feat: Kelvin conversion method added

--- ChambeaPe/WeatherForecast.cs
+++ ChambeaPe/WeatherForecast.cs
@@ -8,5 +8,7 @@ public class WeatherForecast
 
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+    public int TemperatureK => (int)(((TemperatureF - 32) / 1.8) + 273.15);
+
     public string? Summary { get; set; }
 }
